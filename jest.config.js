cb14a2abb99a06cbceb4c112ac5b8029b842872a
Nicola Zambello <nzambello@protonmail.com>
2021-05-19 10:55:50 +0200

    chore: add test and release env + config

--- jest.config.js
+++ jest.config.js
@@ -1,20 +1,15 @@
 module.exports = {
   testEnvironment: 'node',
   transform: {
-    "^.+\\.tsx?$": "ts-jest"
+    '^.+\\.tsx?$': 'ts-jest',
   },
-  moduleFileExtensions: [
-    "ts",
-    "tsx",
-    "js",
-    "jsx",
-    "json",
-    "node",
-  ],
-  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.(ts|js)x?$',
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  resetMocks: true,
+  testRegex: '(/src/.*|\\.|/)(test|spec)\\.(ts|js)x?$',
   coverageDirectory: 'coverage',
   collectCoverageFrom: [
     'src/**/*.{ts,tsx,js,jsx}',
     '!src/**/*.d.ts',
+    '!src/**/*.test.{ts,tsx,js,jsx}',
   ],
 };
